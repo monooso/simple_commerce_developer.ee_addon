6ebc3ea702ea6c08c262b5ec7d43620a463d1509
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-01-24 17:51:21 +0000

    Implement 'Build IPN Call' view.

--- third_party/simple_commerce_developer/views/mod_build_ipn_call.php
+++ third_party/simple_commerce_developer/views/mod_build_ipn_call.php
@@ -1,1 +1,28 @@
-<p>This is a CP page in the Simple Commerce Developer module.</p>
+<?php
+
+echo form_open($form_action);
+
+$this->table->set_template($cp_pad_table_template);
+$this->table->set_heading('Setting', 'Value');
+
+$this->table->add_row(
+  '<label for="products">Products</label>',
+  form_dropdown('products', $products)
+);
+
+$this->table->add_row(
+  '<label for="members">Members</label>',
+  form_dropdown('members', $members)
+);
+
+echo $this->table->generate();
+
+echo form_submit(array(
+  'class'   => 'submit',
+  'name'    => 'submit',
+  'value'   => 'Execute IPN Call'
+));
+
+echo form_close();
+
+?>
